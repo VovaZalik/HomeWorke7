ca64f63c9a9372651e12dfd57cd71aed90274209
VovaZ <zalikvova1998@gmail.com>
2023-05-16 16:49:24 +0300

    FF

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        long minors = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+
+        List<String> namesOfConscriptspersons = persons.stream()
+                .filter(person -> person.getAge() < 27)
+                .filter(person -> person.getAge() >= 18)
+                .map(Person::getFamily)
+                .toList();
+
+        List<Person> employee = persons.stream()
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> person.getSex() == Sex.MAN && person.getAge() < 65
+                || person.getSex() == Sex.WOMAN && person.getAge() < 60)
+                .filter(person -> person.getEducation() == Education.HIGHER)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .toList();
+    }
+}
